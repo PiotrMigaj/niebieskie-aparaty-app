e26c7831c088f650e53739d3d58828b16da2732e
pmigaj <pm@nitzinger-it.com>
2025-04-09 18:08:20 +0200

    add cookie options

--- server/api/login.post.js
+++ server/api/login.post.js
@@ -5,11 +5,22 @@ export default defineEventHandler(async (event) => {
   const decodedCredentials = atob(credentials);
   const [username, password] = decodedCredentials.split(":");
   const authService = AuthServiceFactory.getInstance();
-  const authUser: AuthUser | null = await authService.authenticate({
+  const authUser = await authService.authenticate({
     username: username,
     password: password,
   });
   if (authUser) {
+    const cookieOptions = {
+      cookie: {
+        domain: '.niebieskie-aparaty.pl', // Set the cookie domain
+        path: '/',                 // Set the cookie path
+        maxAge: 60 * 60 * 24 * 7,  // Set cookie expiration (e.g., 7 days in seconds)
+        secure: false,              // Ensure the cookie is sent over HTTPS
+        httpOnly: true,            // Prevent client-side JavaScript access
+        sameSite: 'lax',           // CSRF protection
+      }
+     };
+    console.log(cookieOptions);
     await setUserSession(
       event,
       {
@@ -17,9 +28,7 @@ export default defineEventHandler(async (event) => {
           username: authUser.username,
         },
       },
-      {
-        maxAge: 60 * 60 * 24 * 7,
-      }
+      cookieOptions
     );
     return {};
   } else {
